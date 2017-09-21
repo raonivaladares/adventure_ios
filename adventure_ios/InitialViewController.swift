c7828fe2a2b7e4640543ae24d8994a7811000b8c
rov <valadares.thel@gmail.com>
2017-09-20 21:44:31 -0300

    Adds new initial screen

--- adventure_ios/InitialViewController.swift
+++ adventure_ios/InitialViewController.swift
@@ -1,6 +1,21 @@
 import UIKit
 
 class InitialViewController: UIViewController {
+	private let scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		
+		return scrollView
+	}()
+	
+	private let contentView: UIView = {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		
+		return view
+	}()
+	
+	
 	private let labelTitle: UILabel = {
 		let label = UILabel()
 		label.text = "Adventure"
@@ -19,29 +34,78 @@ class InitialViewController: UIViewController {
 		return imageView
 	}()
 	
+	private let buttonNewGame: UIButton = {
+		let button = UIButton()
+		button.setTitle("New Game", for: .normal)
+		button.setTitleColor(.black, for: .normal)
+		button.backgroundColor = UIColor(red: 41/256, green: 128/256, blue: 185/256, alpha: 1)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		
+		return button
+	}()
+	
+	private let buttonLoadGame: UIButton = {
+		let button = UIButton()
+		button.setTitle("Load Game", for: .normal)
+		button.setTitleColor(.black, for: .normal)
+		button.backgroundColor = UIColor(red: 41/256, green: 128/256, blue: 185/256, alpha: 1)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		
+		return button
+	}()
+	
 	override func viewDidLoad() {
-		view.backgroundColor = UIColor.init(red: 217/256, green: 219/256, blue: 205/256, alpha: 1)
+		view.backgroundColor = UIColor(red: 217/256, green: 219/256, blue: 205/256, alpha: 1)
 		
 		buildUI()
 	}
 	
 	private func buildUI() {
-		view.addSubview(labelTitle)
-		view.addSubview(imageCover)
+		view.addSubview(scrollView)
+		scrollView.addSubview(contentView)
+		
+		contentView.addSubview(labelTitle)
+		
+		contentView.addSubview(imageCover)
+		contentView.addSubview(buttonNewGame)
+		contentView.addSubview(buttonLoadGame)
+		
+		let contentViewHeightAnchor = contentView.heightAnchor.constraint(equalTo: view.heightAnchor)
+		contentViewHeightAnchor.priority = 250
 		
 		NSLayoutConstraint.activate([
+			scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+			scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			
+			contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+			contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+			contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+			contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+			contentView.widthAnchor.constraint(equalTo: view.widthAnchor),
+			contentViewHeightAnchor,
+			
+			labelTitle.topAnchor.constraint(equalTo:  contentView.topAnchor, constant: 50),
+			labelTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+			labelTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
 			labelTitle.heightAnchor.constraint(equalToConstant: 30),
-			labelTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-			labelTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-			labelTitle.topAnchor.constraint(equalTo:  view.topAnchor, constant: 50),
 			
 			imageCover.topAnchor.constraint(equalTo: labelTitle.bottomAnchor, constant: 32),
-			
-			imageCover.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-			imageCover.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+			imageCover.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+			imageCover.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
 			imageCover.heightAnchor.constraint(equalToConstant: 200),
+
+			buttonNewGame.topAnchor.constraint(equalTo: imageCover.bottomAnchor, constant: 60),
+			buttonNewGame.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+			buttonNewGame.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.4),
+			buttonNewGame.heightAnchor.constraint(equalToConstant: 44),
 			
-			
+			buttonLoadGame.topAnchor.constraint(equalTo: buttonNewGame.bottomAnchor, constant: 60),
+			buttonLoadGame.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -16),
+			buttonLoadGame.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+			buttonLoadGame.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.4),
+			buttonLoadGame.heightAnchor.constraint(equalToConstant: 44),
 		])
 	}
 }
